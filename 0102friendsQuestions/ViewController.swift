3ad9554d1117f01ed9886429a016ccc44e4c22be
Lesleytsai9 <e861k91677@gmail.com>
2020-01-02 02:51:20 +0800

    Initial Commit

---
+++ 0102friendsQuestions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0102friendsQuestions
+//
+//  Created by 蔡家雯 on 2020/1/2.
+//  Copyright © 2020 lesley tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
